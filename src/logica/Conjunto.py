e4ea0d4b981e3459abfc0fd3f7d1d08f619238d3
Solange <72656254@continental.edu.pe>
2022-09-19 18:49:48 -0500

    Caso2: Calcular promedio con un conjunto de un elemento (Rojo)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -2,4 +2,4 @@ class Conjunto:
     def __int__(self, conjunto):
         self.__conjunto = conjunto
     def promedio (self):
-        return False
+        return None
--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -2,4 +2,10 @@ import unittest
 from src.logica.Conjunto import Conjunto
 
 class TestConjunto( unittest.TestCase ):
-   pass
+   def test_conjunto_vacio_retornaNone( self ):
+     conjunto=Conjunto([])
+     self.assertIsNone(conjunto.promedio())
+
+   def test_conjunto_unElemento_retornaValorUnicoElemento(self):
+      conjunto = Conjunto([5])
+      self.assertEqual(5,conjunto.promedio())
